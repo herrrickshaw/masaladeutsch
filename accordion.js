27ab3e357e68072ccac489e6e541cc9165fe8ede
Umashankar <umashankar@Umashankars-MacBook-Pro.local>
2026-08-08 22:21:19 +0530

    Add Middle Class to Money Class (Sairam & Srividhya) to author's shelf

--- accordion.js
+++ accordion.js
@@ -128,7 +128,9 @@
     ['Haldiram\u2019s Nagpur Orange Burfee, 500 g', 'Haldiram\u2019s', 'B0BGPR43WQ',
      'https://m.media-amazon.com/images/I/71r0frXkPgL._SX679_.jpg'],
     ['The Argumentative Indian', 'Amartya Sen', '0141012110',
-     'https://m.media-amazon.com/images/I/81jz1oGFymL._SY522_.jpg']
+     'https://m.media-amazon.com/images/I/81jz1oGFymL._SY522_.jpg'],
+    ['Middle Class to Money Class: Simple Secrets to Investing like a Pro', 'Sairam & Srividhya', '1946869066',
+     'https://m.media-amazon.com/images/S/compressed.photo.goodreads.com/books/1489158423i/34525905.jpg']
   ];
   window.gsIdxShelf = function (host) {
     if (!AFFILIATE_TAG || !SHELF.length) return;
